e87b50a2e9e18807ee91a0300c2641569e525eb8
andyjwest <andyjwest@gmail.com>
2020-01-01 12:05:24 -0500

    Adding a bunch of stuff

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import './App.scss';
 import recipes from './recipes';
 import Recipe from './Recipe';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import RecipeCard from "./RecipeCard";
 
 function App() {
-  return (
-    <div className="App">
-      {recipes.map(recipe => Recipe(recipe))}
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                {recipes.map(recipe => <RecipeCard {...recipe} />)}
+            </div>
+        </Router>
+    );
 }
 
 export default App;
+
--- src/Step.js
+++ src/Step.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
 export default function Step({equipment, temperature, description, ingredients, time, priorStepRequired}) {
-  return (
-      <div className='step'>
-        {!!ingredients &&
-        <div>{ingredients.map(ingredient => Ingredients(ingredient))}</div>}
-        <div>{description}</div>
-      </div>
-  );
+    return (
+        <div className='step'>
+            <div>{!!ingredients && <div>
+                {ingredients.map(ingredient => <Ingredients {...ingredient} />)}</div>}</div>
+            <div>{description}</div>
+            <div>
+                {!!equipment && <div>
+                    {equipment.map(it => <div>{it}</div>)}
+                </div>}
+                {!!temperature && <div>Temp: {temperature}</div>}
+                {!!time && <div>Time: {time && `${time.value} ${time.units}`}</div>}
+            </div>
+        </div>
+    );
 }
 
 const Ingredients = ({name, amount, madeInStep}) => <div>
-  <div> {name}</div>
+    <div>{!!amount && `${amount.value} ${amount.units} `}{name} {!!madeInStep &&
+    <sup className='madeInStep'>{madeInStep}</sup>}</div>
 </div>;
