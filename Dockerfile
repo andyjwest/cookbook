bf85efeb91cc05687e4584eff035b357825a6488
Andy West <andyjwest@gmail.com>
2021-02-19 13:43:08 -0500

    Adding a dockerfile

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:13.12.0-alpine as builder
+
+ARG REACT_APP_ENV
+ENV REACT_APP_ENV=${REACT_APP_ENV}
+
+WORKDIR /app
+COPY . .
+RUN npm install react-scripts -g
+RUN npm install
+RUN NODE_ENV=production REACT_APP_ENV=$REACT_APP_ENV npm run build
+
+FROM node:13.12.0-alpine
+RUN npm install serve -g --silent
+WORKDIR /app
+COPY --from=builder /app/build .
+EXPOSE 5002
+CMD ["serve", "-p", "5002", "-s", "."]
---
+++ src/mock/recipes.js
@@ -0,0 +1,25 @@
+export default [
+    {
+        "title": "Key Lime Pie",
+        "id": "key-lime-pie",
+        "description": "Tangy lemons and limes mix makes this pie refreshing and light.",
+        "source": "https://www.foodnetwork.com/recipes/key-lime-pie-recipe1-2011840",
+        "yields": "1 9-inch pie",
+        "ingredients": [
+            {
+                "name": "graham crackers",
+                "amount": {
+                    "value": 150,
+                    "units": "grams"
+                }
+            },
+        ],
+        "steps": [
+            {
+                "description": "Break up the graham crackers; place in a food processor and process to crumbs. If you don't have a food processor, place the crackers in a large plastic bag; seal and then crush the crackers with a rolling pin. Add the melted butter and sugar and pulse or stir until combined."
+            }
+        ],
+        "serving": "Freeze for 15 to 20 minutes before serving. Cut the pie into wedges and serve very cold, topping each wedge with a large dollop of whipped cream.",
+        "titleImage": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2013/11/26/0/BE1B33_Key-Lime-Pie_s4x3.jpg.rend.hgtvcom.826.620.suffix/1386172170553.jpeg"
+    }
+]
